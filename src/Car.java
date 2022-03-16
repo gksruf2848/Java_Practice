051d625c0d2594c2e10691304b3c5f35e0675799
gksruf2848 <4455qqq@naver.com>
2022-03-17 03:03:04 +0900

    .

--- src/Car.java
+++ src/Car.java
@@ -4,6 +4,7 @@ public class Car {
     String model;
     String color;
     int maxSpeed;
+    int gas;
 
     //생성자
     Car() {
@@ -21,5 +22,31 @@ public class Car {
         this.model = model;
         this.color = color;
         this.maxSpeed = maxSpeed;
+    }
+
+    //메소드
+    void setGas(int gas) {
+        this.gas = gas;
+    }
+
+    boolean isLeftGas() {
+        if(gas==0) {
+            System.out.println("gas가 없습니다.");
+            return false;
+        }
+        System.out.println("gas가 있습니다.");
+        return true;
+    }
+
+    void run() {
+        while(true) {
+            if(gas > 0) {
+                System.out.println("달립니다.(gas잔량 : " + gas + ")");
+                gas -= 1;
+            } else {
+                System.out.println("멈춥니다.(gas잔량 : " + gas + ")");
+                return;
+            }
+        }
     }
 }
