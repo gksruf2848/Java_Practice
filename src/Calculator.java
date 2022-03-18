5c631ab94a7e6c4d57e59f5efc8fb873a12545c7
gksruf2848 <4455qqq@naver.com>
2022-03-19 06:07:08 +0900

    .

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,4 +18,9 @@ public class Calculator {
     void println(String message) {
         System.out.println(message);
     }
+
+    double areaCircle(double r) {
+        System.out.println("Calculator 객체의 areaCircle() 실행");
+        return 3.141592 * r * r;
+    }
 }
