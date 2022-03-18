b94c8b47c8da4af325071d51ddba669c194df0c5
gksruf2848 <4455qqq@naver.com>
2022-03-19 02:27:30 +0900

    .

--- src/CarExample.java
+++ src/CarExample.java
@@ -47,6 +47,21 @@ public class CarExample {
         System.out.println("현재 속도 : " + speed + "km/h");
          */
 
+        //잘못된 속도 변경
+        myCar.setSpeed(-50);
 
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+        //올바른 속도 변경
+        myCar.setSpeed(60);
+
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+        //멈춤
+        if(!myCar.isStop()) {
+            myCar.setStop(true);
+        }
+
+        System.out.println("현재 속도 : " + myCar.getSpeed());
     }
 }
