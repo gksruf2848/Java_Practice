219c61ec3e5dcf6ce264c958fcbf198597ed3a5e
gksruf2848 <4455qqq@naver.com>
2022-03-20 02:33:11 +0900

    .

--- src/Car.java
+++ src/Car.java
@@ -8,10 +8,12 @@ public class Car {
     private int speed;
     private boolean stop;
 
-    Tire frontLeftTire = new Tire("앞왼쪽", 6);
-    Tire frontRightTire = new Tire("앞오른쪽", 2);
-    Tire backLeftTire = new Tire("뒤왼쪽", 3);
-    Tire backRightTire = new Tire("뒤오른쪽", 4);
+    Tire[] tires = {
+            new Tire("앞왼쪽", 6),
+            new Tire("앞오른쪽", 2),
+            new Tire("뒤왼쪽", 3),
+            new Tire("뒤오른쪽", 4)
+    };
 
     //생성자
     Car() {
@@ -100,10 +102,12 @@ public class Car {
 
     int run() {
         System.out.println("[자동차가 달립니다.]");
-        if(frontLeftTire.roll()==false) { stop(); return 1; }
-        if(frontRightTire.roll()==false) { stop(); return 2; }
-        if(backLeftTire.roll()==false) { stop(); return 3; }
-        if(backRightTire.roll()==false) { stop(); return 4; }
+        for(int i = 0; i < tires.length; i++) {
+            if(tires[i].roll() == false) {
+                stop();
+                return (i+1);
+            }
+        }
         return 0;
     }
 
