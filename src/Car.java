47c5aa30bceded6502b2033a6f52e28fcf43dd5a
gksruf2848 <4455qqq@naver.com>
2022-03-20 02:13:38 +0900

    .

--- src/Car.java
+++ src/Car.java
@@ -8,6 +8,11 @@ public class Car {
     private int speed;
     private boolean stop;
 
+    Tire frontLeftTire = new Tire("앞왼쪽", 6);
+    Tire frontRightTire = new Tire("앞오른쪽", 2);
+    Tire backLeftTire = new Tire("뒤왼쪽", 3);
+    Tire backRightTire = new Tire("뒤오른쪽", 4);
+
     //생성자
     Car() {
     }
@@ -91,5 +96,18 @@ public class Car {
     public void setStop(boolean stop) {
         this.stop = stop;
         this.speed = 0;
+    }
+
+    int run() {
+        System.out.println("[자동차가 달립니다.]");
+        if(frontLeftTire.roll()==false) { stop(); return 1; }
+        if(frontRightTire.roll()==false) { stop(); return 2; }
+        if(backLeftTire.roll()==false) { stop(); return 3; }
+        if(backRightTire.roll()==false) { stop(); return 4; }
+        return 0;
+    }
+
+    void stop() {
+        System.out.println("[자동차가 멈춥니다.]");
     }
 }
--- src/CarExample.java
+++ src/CarExample.java
@@ -1,5 +1,6 @@
 public class CarExample {
     public static void main(String[] args) {
+        /*
         Car car1 = new Car();
         System.out.println("car1.company : " + car1.company);
         System.out.println();
@@ -22,7 +23,10 @@ public class CarExample {
         System.out.println("car4.color : " + car4.color);
         System.out.println("car4.maxSpeed : " + car4.maxSpeed);
         System.out.println();
+         */
 
+
+        /*
         Car myCar = new Car();
 
         myCar.setGas(5);
@@ -38,6 +42,9 @@ public class CarExample {
         } else {
             System.out.println("gas를 주입하세요.");
         }
+        */
+
+
 
         /*
         //클래스 외부에서 메소드 호출
@@ -47,6 +54,9 @@ public class CarExample {
         System.out.println("현재 속도 : " + speed + "km/h");
          */
 
+
+
+        /*
         //잘못된 속도 변경
         myCar.setSpeed(-50);
 
@@ -63,5 +73,32 @@ public class CarExample {
         }
 
         System.out.println("현재 속도 : " + myCar.getSpeed());
+         */
+
+        Car car = new Car();
+
+        for(int i=1; i<=5; i++) {
+            int problemLocation = car.run();
+
+            switch(problemLocation) {
+                case 1:
+                    System.out.println("앞왼쪽 HankookTire로 교체");
+                    car.frontLeftTire = new HankookTire("앞왼쪽", 15);
+                    break;
+                case 2:
+                    System.out.println("앞오른쪽 KumhoTire로 교체");
+                    car.frontRightTire = new KumhoTire("앞오른쪽", 13);
+                    break;
+                case 3:
+                    System.out.println("뒤왼쪽 HankookTire로 교체");
+                    car.backLeftTire = new HankookTire("뒤왼쪽", 14);
+                    break;
+                case 4:
+                    System.out.println("뒤오른쪽 KumhoTire로 교체");
+                    car.backRightTire = new KumhoTire("뒤오른쪽", 17);
+                    break;
+            }
+            System.out.println("---------------------------------------");
+        }
     }
 }
