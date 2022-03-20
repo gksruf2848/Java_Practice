ba6e1776a31560c6b64355b02b84fc82b6db2340
gksruf2848 <4455qqq@naver.com>
2022-03-21 02:33:04 +0900

    .

--- src/RemoteControlExample.java
+++ src/RemoteControlExample.java
@@ -4,11 +4,15 @@ public class RemoteControlExample {
 
         rc = new Television();
         rc.turnOn();
+        rc.setMute(true);
         rc.turnOff();
 
         rc = new Audio();
         rc.turnOn();
+        rc.setMute(true);
         rc.turnOff();
+
+        RemoteControl.changeBattery();
 
     }
 }
